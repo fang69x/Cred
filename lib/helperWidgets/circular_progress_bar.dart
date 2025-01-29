d807b82407ed61245ded01aa79883233a47f2963
fang69x <109065342+fang69x@users.noreply.github.com>
2025-01-29 16:31:40 +0530

    circular progress indicator improved

--- lib/helperWidgets/circular_progress_bar.dart
+++ lib/helperWidgets/circular_progress_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'dart:ui';
 
 class CreditCardWidget extends StatefulWidget {
   final double minRange;
@@ -23,13 +24,33 @@ class CreditCardWidget extends StatefulWidget {
   _CreditCardWidgetState createState() => _CreditCardWidgetState();
 }
 
-class _CreditCardWidgetState extends State<CreditCardWidget> {
+class _CreditCardWidgetState extends State<CreditCardWidget>
+    with SingleTickerProviderStateMixin {
   late double currentValue;
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
 
   @override
   void initState() {
     super.initState();
     currentValue = widget.minRange;
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.02,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   void updateCurrentValue(double value) {
@@ -44,137 +65,188 @@ class _CreditCardWidgetState extends State<CreditCardWidget> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Material(
-      elevation: 5,
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        height: screenHeight * 0.5,
-        width: screenWidth * 0.9,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 15,
-              offset: const Offset(0, 4),
+    return MouseRegion(
+      onEnter: (_) => _animationController.forward(),
+      onExit: (_) => _animationController.reverse(),
+      child: AnimatedBuilder(
+        animation: _scaleAnimation,
+        builder: (context, child) => Transform.scale(
+          scale: _scaleAnimation.value,
+          child: Container(
+            height: screenHeight * 0.5,
+            width: screenWidth * 0.9,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  const Color(0xFF1A1A2E).withOpacity(0.95),
+                  const Color(0xFF0A2F4D).withOpacity(0.90),
+                ],
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 20,
+                  offset: const Offset(0, 10),
+                ),
+                BoxShadow(
+                  color: Colors.blue.withOpacity(0.1),
+                  blurRadius: 30,
+                  offset: const Offset(-10, -10),
+                ),
+              ],
             ),
-          ],
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: screenWidth * 0.05,
-            vertical: screenHeight * 0.02,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                flex: 4,
-                child: SfRadialGauge(
-                  axes: <RadialAxis>[
-                    RadialAxis(
-                      minimum: widget.minRange,
-                      maximum: widget.maxRange,
-                      startAngle: 270,
-                      endAngle: 270,
-                      showLabels: false,
-                      showTicks: false,
-                      axisLineStyle: AxisLineStyle(
-                        cornerStyle: CornerStyle.bothFlat,
-                        color: Colors.black12,
-                        thickness: 15,
-                      ),
-                      pointers: <GaugePointer>[
-                        RangePointer(
-                          value: currentValue,
-                          cornerStyle: CornerStyle.bothFlat,
-                          width: 15,
-                          sizeUnit: GaugeSizeUnit.logicalPixel,
-                          color: const Color.fromARGB(255, 144, 101, 36),
-                        ),
-                        NeedlePointer(
-                          value: currentValue,
-                          needleColor: Colors.black,
-                          knobStyle: KnobStyle(
-                            color: Colors.white,
-                            borderColor:
-                                const Color.fromARGB(255, 255, 255, 255),
-                            borderWidth: 3,
-                          ),
-                        ),
-                      ],
-                      annotations: <GaugeAnnotation>[
-                        GaugeAnnotation(
-                          angle: 90,
-                          positionFactor: 0.1,
-                          widget: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Flexible(
-                                child: Text(
-                                  widget.header,
-                                  style: TextStyle(
-                                    fontSize: screenWidth * 0.05,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.1),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(screenWidth * 0.04),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 4,
+                          child: SfRadialGauge(
+                            axes: <RadialAxis>[
+                              RadialAxis(
+                                minimum: widget.minRange,
+                                maximum: widget.maxRange,
+                                startAngle: 270,
+                                endAngle: 270,
+                                showLabels: false,
+                                showTicks: false,
+                                axisLineStyle: AxisLineStyle(
+                                  cornerStyle: CornerStyle.bothFlat,
+                                  color: Colors.white.withOpacity(0.1),
+                                  thickness: 15,
+                                ),
+                                pointers: <GaugePointer>[
+                                  RangePointer(
+                                    value: currentValue,
+                                    cornerStyle: CornerStyle.bothFlat,
+                                    width: 15,
+                                    sizeUnit: GaugeSizeUnit.logicalPixel,
+                                    gradient: const SweepGradient(
+                                      colors: [
+                                        Color(0xFFFFD700),
+                                        Color.fromARGB(255, 235, 131, 3),
+                                      ],
+                                    ),
                                   ),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                ),
-                              ),
-                              SizedBox(height: screenHeight * 0.005),
-                              Text(
-                                '₹${currentValue.toInt()}',
-                                style: TextStyle(
-                                  fontSize: screenWidth * 0.055,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              SizedBox(height: screenHeight * 0.01),
-                              Flexible(
-                                child: Text(
-                                  widget.description,
-                                  style: TextStyle(
-                                    fontSize: screenWidth * 0.035,
-                                    color:
-                                        const Color.fromARGB(255, 12, 118, 60)
-                                            .withOpacity(0.6),
+                                  NeedlePointer(
+                                    value: currentValue,
+                                    needleColor: Colors.white,
+                                    needleLength: 0.6,
+                                    knobStyle: KnobStyle(
+                                      color: Colors.white,
+                                      borderColor: const Color.fromARGB(
+                                          255, 255, 153, 0),
+                                      borderWidth: 2,
+                                      knobRadius: 0.08,
+                                    ),
+                                    tailStyle: const TailStyle(
+                                      color: Color(0xFFFFD700),
+                                      width: 4,
+                                      length: 0.2,
+                                    ),
                                   ),
-                                  textAlign: TextAlign.center,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
-                                ),
+                                ],
+                                annotations: <GaugeAnnotation>[
+                                  GaugeAnnotation(
+                                    angle: 90,
+                                    positionFactor: 0.1,
+                                    widget: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Text(
+                                          widget.header,
+                                          style: TextStyle(
+                                            fontSize: screenWidth * 0.045,
+                                            fontWeight: FontWeight.w600,
+                                            color:
+                                                Colors.white.withOpacity(0.9),
+                                          ),
+                                        ),
+                                        SizedBox(height: screenHeight * 0.01),
+                                        Text(
+                                          '₹${currentValue.toInt().toString().replaceAllMapped(
+                                                RegExp(
+                                                    r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+                                                (Match m) => '${m[1]},',
+                                              )}',
+                                          style: TextStyle(
+                                            fontSize: screenWidth * 0.06,
+                                            fontWeight: FontWeight.bold,
+                                            color: const Color.fromARGB(
+                                                172, 2, 36, 149),
+                                          ),
+                                        ),
+                                        SizedBox(height: screenHeight * 0.015),
+                                        Text(
+                                          widget.description,
+                                          style: TextStyle(
+                                            fontSize: screenWidth * 0.035,
+                                            color: const Color.fromARGB(
+                                                    255, 0, 25, 250)
+                                                .withOpacity(0.8),
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
                           ),
                         ),
+                        Text(
+                          widget.footer,
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.035,
+                            color: Colors.white.withOpacity(0.7),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: screenHeight * 0.02),
+                        SliderTheme(
+                          data: SliderThemeData(
+                            trackHeight: 4,
+                            activeTrackColor: const Color(0xFFFFD700),
+                            inactiveTrackColor: Colors.white.withOpacity(0.1),
+                            thumbColor: Colors.white,
+                            overlayColor:
+                                const Color(0xFFFFD700).withOpacity(0.2),
+                            thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 8,
+                            ),
+                            overlayShape: const RoundSliderOverlayShape(
+                              overlayRadius: 16,
+                            ),
+                          ),
+                          child: Slider(
+                            value: currentValue,
+                            min: widget.minRange,
+                            max: widget.maxRange,
+                            onChanged: updateCurrentValue,
+                          ),
+                        ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-              SizedBox(height: screenHeight * 0.01),
-              Text(
-                widget.footer,
-                style: TextStyle(
-                  fontSize: screenWidth * 0.035,
-                  color: Colors.black.withOpacity(0.6),
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.02),
-              Slider(
-                value: currentValue,
-                min: widget.minRange,
-                max: widget.maxRange,
-                onChanged: (value) {
-                  updateCurrentValue(value);
-                },
-                activeColor: const Color.fromARGB(255, 144, 101, 36),
-                inactiveColor: Colors.black12,
-              ),
-            ],
+            ),
           ),
         ),
       ),
